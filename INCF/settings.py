bb8d4490954621b53f2527f5db7d69bc98b3d77a
Om Khare <omkhare@Oms-MacBook-Air.local>
2024-03-03 20:05:29 +0530

    deployment changes

--- INCF/settings.py
+++ INCF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-osyyqtnpxgv)v8fi@$7sj-mj8g60u!2lj^qdftn8ur%4w5n*^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "INCF.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER":"postgres",
+        "PASSWORD":"5*ee1*fdeG*-DDgAdggGDCG5F6cFcDEF",
+        "HOST":"roundhouse.proxy.rlwy.net",
+        "PORT":"55730",
     }
 }
 
@@ -118,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIR =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
